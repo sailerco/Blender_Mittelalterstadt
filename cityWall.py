a74ffa3d13ec03c0e13ffb05335bf44cd577b7f0
TaJaAu <tamara.janine.auber@hs-furtwangen.de>
2022-02-09 16:26:07 +0100

    kreis

--- cityWall.py
+++ cityWall.py
@@ -1,10 +1,11 @@
+from pickle import FALSE
 import bpy
 import math
 
 
 class cityWall():
 
-    TOWER_COUNT = 10
+    TOWER_COUNT = 5
     RADIUS = 50
     
     tower_height = 12
@@ -24,11 +25,11 @@ class cityWall():
     GATE_HEIGHT = 1
     GATE_WIDTH = 1.4
     WALL_WIDTH = wall_thickness
-    
-    x = []
-    y = []
-   
-
+    kreis = False
+    if(kreis):
+        wall_vertices = 32
+    else:
+        wall_vertices = TOWER_COUNT
 
 ## generiert einzelnen Turm
     def generate_base(self, tower_location, tower_height):
@@ -71,8 +72,6 @@ class cityWall():
             x_value = math.sin(2*math.pi/self.TOWER_COUNT * i) * self.RADIUS
             y_value = math.cos(2*math.pi/self.TOWER_COUNT * i) * self.RADIUS
             tower_location=(x_value, y_value, self.tower_height/2)
-            self.x.append(x_value)
-            self.y.append(y_value)
             self.generate_base(tower_location, self.tower_height)
 
             t = bpy.context.object
@@ -81,27 +80,23 @@ class cityWall():
     
     def generateGates(self):
         print("start")
-        x1 = self.x[0]
-        y1 = self.y[0]
-        for i in range(len(self.x)+1):
+        for i in range(self.TOWER_COUNT+1):
             x_value = math.sin(2*math.pi/self.TOWER_COUNT * (i + 0.5)) * self.RADIUS -1
             y_value = math.cos(2*math.pi/self.TOWER_COUNT * (i +0.5)) * self.RADIUS -1
-            # if(i != len(self.x)):
-            #     print(i)
-            #     x_value = self.x[i+1]-self.x[i]
-            #     y_value = self.y[i+1]-self.y[i]
-            # else:
-            #     print("10")
-            #     x_value = self.x[0]-self.x[i]
-            #     y_value = self.x[0]-self.x[i]
+           
             gate_cube = bpy.ops.mesh.primitive_cube_add(location=(x_value, y_value, 0), scale=(self.WALL_WIDTH + 1, self.GATE_WIDTH, self.GATE_HEIGHT))
             bpy.ops.transform.rotate(value=(2*math.pi/self.TOWER_COUNT * i+math.pi/self.TOWER_COUNT) + 1.5708, orient_axis='Z', orient_type='GLOBAL')
             gate_cylinder = bpy.ops.mesh.primitive_cylinder_add(enter_editmode=False, align='WORLD', location=(x_value, y_value, self.GATE_HEIGHT), scale=(1, self.GATE_WIDTH, self.WALL_WIDTH + 1))
             bpy.ops.transform.rotate(value=1.5708, orient_axis='Y', orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=True, use_proportional_edit=False, proportional_edit_falloff='SMOOTH', proportional_size=1, use_proportional_connected=False, use_proportional_projected=False)
             bpy.ops.transform.rotate(value=(2*math.pi/self.TOWER_COUNT * i+math.pi/self.TOWER_COUNT) + 1.5708, orient_axis='Z', orient_type='GLOBAL')
+
+
+
+
 ##generiert Stadtmauer
     def generate_wall(self):
-        bpy.ops.mesh.primitive_cylinder_add(vertices=self.TOWER_COUNT, radius=self.RADIUS, depth=self.wall_height, location=(0, 0, self.wall_height/2))
+        bpy.ops.mesh.primitive_cylinder_add(vertices=self.wall_vertices, radius=self.RADIUS, depth=self.wall_height, location=(0, 0, self.wall_height/2))
+       
 
 
         mat_wall = bpy.data.materials.new("material_wall")
@@ -128,18 +123,21 @@ class cityWall():
 
 
         cube_contextBigC = bpy.context.object 
-        bpy.ops.mesh.primitive_cylinder_add(vertices=self.TOWER_COUNT, radius=self.INNER_RADIUS, depth=self.wall_height, location=(0, 0, self.wall_height/2))
+        bpy.ops.mesh.primitive_cylinder_add(vertices=self.wall_vertices, radius=self.INNER_RADIUS, depth=self.wall_height, location=(0, 0, self.wall_height/2))
+       
         bpy.context.object.display_type = 'WIRE'
 
         cube_context = bpy.context.object          
         boolean_mod = cube_contextBigC.modifiers.new("boolean", "BOOLEAN")
         boolean_mod.object = cube_context
+
         ##bpy.context.object.modifiers["boolean"].operation = 'DIFFERENCE'
         #bpy.ops.object.modifier_apply(apply_as='DATA', modifier="boolean")
 
 
 
-        bpy.ops.mesh.primitive_cylinder_add(vertices=self.TOWER_COUNT, radius=self.RADIUS + self.RADIUS*0.01, depth=self.wall_height*0.02, location=(0, 0, self.wall_height))
+        bpy.ops.mesh.primitive_cylinder_add(vertices=self.wall_vertices, radius=self.RADIUS + self.RADIUS*0.01, depth=self.wall_height*0.02, location=(0, 0, self.wall_height))
+        
         
         mat_wall_top = bpy.data.materials.new("material_wall_top")
         mat_wall_top.use_nodes = True
@@ -147,7 +145,8 @@ class cityWall():
         bpy.context.object.data.materials.append(mat_wall_top) 
         
         cylinder_top = bpy.context.object 
-        bpy.ops.mesh.primitive_cylinder_add(vertices=self.TOWER_COUNT, radius=self.INNER_RADIUS, depth=self.wall_height*0.02, location=(0, 0, self.wall_height))
+        bpy.ops.mesh.primitive_cylinder_add(vertices=self.wall_vertices, radius=self.INNER_RADIUS, depth=self.wall_height*0.02, location=(0, 0, self.wall_height))
+       
         bpy.context.object.display_type = 'WIRE'
         cylinder_top_inner = bpy.context.object
         boolean_cylinder_top = cylinder_top.modifiers.new("booleantop", "BOOLEAN")
