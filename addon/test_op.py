4dcdba1992c5624401f695fa7cad95aebd036d58
JaninacpB <61713354+JaninacpB@users.noreply.github.com>
2022-02-09 20:52:50 +0100

    gernate button funktioniert

--- addon/test_op.py
+++ addon/test_op.py
@@ -2,8 +2,8 @@ import bpy
 from bpy.types import Operator
 
 PROPS=  [
-            ('towerCount', bpy.props.IntProperty(name = 'Tower', description='int value of Towers', default=7, min=4, max=15)),
-            ('radius', bpy.props.IntProperty(name = 'Radius', description='int value of Radius', default=25, min=4, max=95)),
+            ('towerCount', bpy.props.IntProperty(name = 'Tower', description='int value of Towers', default=7, min=5, max=15)),
+            ('radius', bpy.props.IntProperty(name = 'Radius', description='int value of Radius', default=40, min=10, max=60)),
             ('hasChurch', bpy.props.BoolProperty(name='Show church', default=True)),
             ('isRound', bpy.props.BoolProperty(name='Round shape', default=False))
         ]
@@ -12,20 +12,8 @@ class test_OT_test(Operator):
     bl_idname = "object.apply_all_mods"
     bl_label = "Apply all"
     bl_description = "Apply all operators of the active object"
-
-    @classmethod
-    def poll(cls, context):
-        obj = context.object 
-
-        if obj is not None:
-            if obj.mode == "OBJECT":
-                return True
-            return False
         
     def execute(self, context):
-        active_obj = context.view_layer.objects.active
-
-        for mod in active_obj.modifiers: 
-            bpy.ops.object.modifier_apply(modifier=mod.name)
+        print('Test, Testclasse')
 
         return{'FINISHED'}
